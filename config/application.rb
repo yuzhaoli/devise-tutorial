15b7580116f40bdeacbafd242f2d12e6aabfafb3
yuzhaoli <li.3396@osu.edu>
2016-11-18 00:05:18 -0500

    first commit

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DeviseTutorial
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+
+
+    ActionMailer::Base.smtp_settings = {
+      :address      => 'smtp.gmail.com',
+      :domain       => 'mail.google.com',
+      :port         => 587,
+      :user_name    => 'lee.1381@gmail.com', #ENV['GMAIL_USERNAME']
+      :password     => 'Vnfjri.3396',
+      :authentication => 'login',
+      :enable_starttls_auto => true     
+    }
+  end
+end
